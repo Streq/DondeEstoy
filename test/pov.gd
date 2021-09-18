12d08219667bfd2c923da46280f3a8e358a02a8b
streq <strequena@gmail.com>
2021-09-18 11:51:00 -0300

    booleano para que no se mueva en el editor el pov en el test de fov gov lov

--- test/pov.gd
+++ test/pov.gd
@@ -1,7 +1,10 @@
 tool
 extends Position2D
 
+export (bool) var move_in_editor:=true
+
 
 func _process(delta):
-	position = get_global_mouse_position()
-	update()
+	if move_in_editor or !Engine.editor_hint:
+		position = get_global_mouse_position()
+		update()
