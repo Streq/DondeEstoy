30ad7fe786c3e629b7380ec2612a106f3ce09c05
streq <strequena@gmail.com>
2021-09-22 09:29:02 -0300

    I dunno what this is

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -5,7 +5,7 @@ var look_dir := Vector2.RIGHT
 
 var gravity := 20000
 var run_max_speed := 10000
-var jump_speed := 11000
+var jump_speed := 8000
 
 func _physics_process(delta):
 	var move_dir = float(Input.is_action_pressed("right"))-float(Input.is_action_pressed("left"))
@@ -14,11 +14,11 @@ func _physics_process(delta):
 	velocity.y += gravity*delta
 	
 	var vel = move_and_slide(velocity*delta, Vector2.UP)
-	if !vel.x:
-		velocity.x = 0
-	if is_on_floor() or is_on_ceiling():
+	
+	if is_on_floor() and velocity.y > 0 or is_on_ceiling() and velocity.y < 0:
 		velocity.y = 0
-
+	if is_on_wall():
+		velocity.x = 0
 
 func _input(event):
 	if event.is_action_pressed("A"):
