a5a765855d2741cae38b5a2a535e439a0ea78588
streq <strequena@gmail.com>
2021-09-23 07:45:09 -0300

    this is as complex as the main character will get, I make this promise to myself

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -7,19 +7,50 @@ var gravity := 20000
 var run_max_speed := 10000
 var jump_speed := 8000
 
+var air := false
+
+var ground_friction = 10
+var air_movement = 0.5
+
+var jump := false
+
 func _physics_process(delta):
 	var move_dir = float(Input.is_action_pressed("right"))-float(Input.is_action_pressed("left"))
-	velocity.x = lerp(velocity.x, run_max_speed*move_dir, delta)
+	
+	if move_dir:
+		if !air:
+			velocity.x = lerp(velocity.x, run_max_speed*move_dir, delta)
+		else:
+			velocity.x = lerp(velocity.x, run_max_speed*move_dir, delta*air_movement)
+	elif !air:
+		velocity.x = lerp(velocity.x, 0, delta*ground_friction)
+	
+	if jump:
+		jump = false
+		if !air:
+			velocity.y -= jump_speed
+	
+	
+	$Sprite.scale.x = look_dir.x
 	
 	velocity.y += gravity*delta
 	
 	var vel = move_and_slide(velocity*delta, Vector2.UP)
 	
-	if is_on_floor() and velocity.y > 0 or is_on_ceiling() and velocity.y < 0:
+	air = !is_on_floor()
+	
+	if !air and velocity.y > 0 or is_on_ceiling() and velocity.y < 0:
 		velocity.y = 0
 	if is_on_wall():
 		velocity.x = 0
+	if air:
+		$AnimationPlayer.play("air")
+	elif move_dir:
+		look_dir.x = move_dir
+		$AnimationPlayer.play("run")
+	else:
+		$AnimationPlayer.play("idle")
 
 func _input(event):
 	if event.is_action_pressed("A"):
-		velocity.y -= jump_speed
+		jump = true
