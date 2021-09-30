0e6ebc325057a8e4f105e2841eab554ae39ae837
streq <strequena@gmail.com>
2021-09-30 15:02:36 -0300

    death is no longer separate from game

--- scenes/death/death.gd
+++ scenes/death/death.gd
@@ -7,11 +7,9 @@ var retry = false
 func _ready():
 	if !Globals.died_once: 
 		yield(get_tree().create_timer(1.0),"timeout")
-	Globals.died_once = true
 	retry = true
 	$retry.visible = true
 	
 func _input(event):
 	if retry and event.is_action_pressed("A"):
-		Signals.emit_signal("pop_scene")
 		Signals.emit_signal("restart_level")
