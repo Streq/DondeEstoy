d2e1fdbdb17924e6b4ce4f7b46fe0c7a1050ba6b
streq <strequena@gmail.com>
2021-10-22 06:37:51 -0300

    face appears for longer

---
+++ levels/giant_creep/just_the_face.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	yield(get_tree().create_timer(2.0),"timeout")
+	Signals.emit_signal("next_level")
